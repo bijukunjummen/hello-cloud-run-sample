b58a32ae8e4b23979c29bbd51bc9f73336e115aa
Biju Kunjummen <biju.kunjummen@gmail.com>
2022-04-28 18:36:34 -0700

    With an exception message

--- src/main/kotlin/org/bk/web/GreetingsController.kt
+++ src/main/kotlin/org/bk/web/GreetingsController.kt
@@ -1,6 +1,7 @@
 package org.bk.web
 
 import org.bk.web.model.Message
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -10,11 +11,23 @@ class GreetingsController {
 
     @GetMapping("/greetings")
     fun greetings(): Message {
+        LOGGER.info("Greetings invoked. Responding with hello")
         return Message("hello")
     }
 
     @GetMapping("/greetings/{payload}")
     fun greetings(@PathVariable("payload") payload: String): Message {
+        LOGGER.info("Greetings invoked. Responding with $payload")
         return Message(payload)
+    }
+
+    @GetMapping("/greetings/exception")
+    fun greetingsWithException(): Message {
+        LOGGER.info("Greetings invoked. About to throw an exception")
+        throw RuntimeException("Throwing an explicit exception")
+    }
+
+    companion object {
+        val LOGGER = LoggerFactory.getLogger(GreetingsController::class.java)
     }
 }
